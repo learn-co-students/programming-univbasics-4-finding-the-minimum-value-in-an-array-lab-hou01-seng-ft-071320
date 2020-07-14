9af565b59fba2bc278757d9124903f463730f50e
Kiri Um <kirirotha@gmail.com>
2020-07-14 12:51:30 +0000

    Done.

--- lib/find_the_minimum_value.rb
+++ lib/find_the_minimum_value.rb
@@ -1,3 +1,11 @@
 def find_min_value(array)
   # Add your solution here
+  number = 99999999999
+  array.each do |index|
+    if index < number
+      number = index
+    end
+  end
+  return number
+    
 end
