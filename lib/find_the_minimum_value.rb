946abc1419bc66047ecfbd00e46a5c4943b06490
github username <github email address>
2020-07-13 21:40:07 -0500

    Done.

--- lib/find_the_minimum_value.rb
+++ lib/find_the_minimum_value.rb
@@ -1,3 +1,12 @@
 def find_min_value(array)
   # Add your solution here
+  min_value = 100
+  counter = 0
+  while counter < array.length do
+    if array[counter] < min_value  
+      min_value = array[counter]
+    end
+    counter +=1 
+  end
+  return min_value
 end
